4727e47ed3c2681aac09e68a76a9605d0ade9d25
Victor Roest <victor@xirion.net>
2020-10-11 19:57:47 +0200

    cleanup

--- services/proxy/bird.go
+++ services/proxy/bird.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"bytes"
+	"github.com/pkg/errors"
 	"io"
 	"net"
 	"net/http"
@@ -14,80 +15,46 @@ func isNumeric(b byte) bool {
 	return b >= byte('0') && b <= byte('9')
 }
 
-func birdScanToEnd(s *bufio.Scanner, w io.Writer) {
+func birdScanToEnd(s *bufio.Scanner, w io.Writer) error {
+	if w == nil {
+		return errors.New("nil writer")
+	}
+
 	for s.Scan() {
 		c := s.Bytes()
 
 		if len(c) > 4 && isNumeric(c[0]) && isNumeric(c[1]) && isNumeric(c[2]) && isNumeric(c[3]) {
-			if w != nil {
-				nc := c[5:]
-				nc = bytes.Trim(nc, " ")
-				nc = append(nc, '\n')
+			nc := c[5:]
+			nc = bytes.Trim(nc, " ")
 
-				if _, err := w.Write(nc); err != nil {
-					panic(err)
-				}
+			if _, err := w.Write(nc); err != nil {
+				return errors.Wrap(err, "error writing to writer")
+			}
+			if _, err := w.Write([]byte{'\n'}); err != nil {
+				return errors.Wrap(err, "error writing newline to writer")
 			}
 
 			// 0xxx = success
 			// 8xxx = failure
 			// 9xxx = failure
 			if c[0] == byte('0') || c[0] == byte('8') || c[0] == byte('9') {
-				return
+				break
 			}
 		} else {
-			if w != nil {
-				c = bytes.Trim(c, " ")
-				c = append(c, '\n')
-				if _, err := w.Write(c); err != nil {
-					panic(err)
-				}
-			}
-		}
-	}
-}
-
-// Read a line from bird socket, removing preceding status number, output it.
-// Returns if there are more lines.
-func birdReadln(bird *bufio.Reader, w io.Writer) bool {
-	c, err := bird.ReadBytes('\n')
-	if err != nil {
-		panic(err)
-	}
-
-	// Check if it starts with a status code (4 digits)
-	if len(c) > 4 && isNumeric(c[0]) && isNumeric(c[1]) && isNumeric(c[2]) && isNumeric(c[3]) {
-		if w != nil {
-			nc := c[5:]
-			nc = bytes.Trim(nc, " ")
-
-			if _, err := w.Write(nc); err != nil {
-				panic(err)
-			}
-		}
-
-		// 0xxx = success
-		// 8xxx = failure
-		// 9xxx = failure
-		return c[0] != byte('0') && c[0] != byte('8') && c[0] != byte('9')
-	} else {
-		if w != nil {
 			c = bytes.Trim(c, " ")
-			if _, err := w.Write(c); err != nil {
-				panic(err)
+			c = append(c, '\n')
+			if _, err := w.Write([]byte{'\n'}); err != nil {
+				return errors.Wrap(err, "error writing to writer")
 			}
 		}
-
-		return true
 	}
+	return nil
 }
 
 // Write a command to a bird socket
-func birdWriteln(bird io.Writer, s string) {
+func birdWriteln(bird io.Writer, s string) error {
 	_, err := bird.Write([]byte(s + "\n"))
-	if err != nil {
-		panic(err)
-	}
+	return errors.Wrap(err, "sending message to bird failed")
 }
 
 func birdHandler(socket string) http.HandlerFunc {
@@ -97,24 +64,49 @@ func birdHandler(socket string) http.HandlerFunc {
 			invalidHandler(w, r)
 			return
 		}
-		// Initialize BIRDv4 socket
+
+		// Connect to BIRDv4 socket
 		sock, err := net.Dial("unix", socket)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
 		defer sock.Close()
+
+		// Set deadline to 30s
 		if err = sock.SetDeadline(time.Now().Add(time.Second * 30)); err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
 
+		// Create scanner for the socket
 		bird := bufio.NewScanner(sock)
 
+		// Advance beyond the welcome message
 		bird.Scan()
-		birdWriteln(sock, "restrict")
+
+		// Restrict access so no modifications can take place
+		err = birdWriteln(sock, "restrict")
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		// Advance over reply
 		bird.Scan()
-		birdWriteln(sock, query)
-		birdScanToEnd(bird, w)
+
+		// Send query to bird
+		err = birdWriteln(sock, query)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		// writeback reply
+		err = birdScanToEnd(bird, w)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
 	}
 }
